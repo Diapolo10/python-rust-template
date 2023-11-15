11c542da5a1f2fb8e05f581216f1a0937101dc29
Lari Liuhamo <lari.liuhamo@gmail.com>
2023-11-15 03:02:52 +0200

    [PATCH] Dependency updates, cleanup

--- python/project_name/__init__.py
+++ python/project_name/__init__.py
@@ -1,3 +1,5 @@
 """Stuff 'n' things."""
 
-from .project_name import *  # type: ignore  # noqa: PGH003, TID252
+from project_name.project_name import *  # type: ignore  # noqa: PGH003
+
+# NOTE: Look for a way to differentiate the names
